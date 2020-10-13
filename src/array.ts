23209b29e4f8b779082ae8c732aea03a85e662e4
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-13 17:04:40 +0200

    arrays

---
+++ src/array.ts
@@ -0,0 +1,18 @@
+var alphas: string[];
+alphas = ["1", "2", "3", "4"];
+
+console.log(alphas[0]);
+console.log(alphas[1]);
+
+var arr_names: number[] = new Array(4);
+
+for (var i = 0; i < arr_names.length; i++) {
+  arr_names[i] = i * 2;
+  console.log(arr_names[i]);
+}
+
+var names: string[] = new Array("Mary", "Tom", "Jack", "Jill");
+
+for (var i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
