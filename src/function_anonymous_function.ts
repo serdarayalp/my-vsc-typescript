092ae651445c214245af540d89c7c49d4d774106
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-13 11:07:48 +0200

    functions

---
+++ src/function_anonymous_function.ts
@@ -0,0 +1,13 @@
+// Function Expression
+var msg = function () {
+    return "hello world";
+  };
+  
+  console.log(msg());
+  
+  var res = function (a: number, b: number) {
+    return a * b;
+  };
+  
+  console.log(res(12, 2));
+  
--- src/function_function_constructor.ts
+++ src/function_function_constructor.ts
@@ -1,3 +1,4 @@
 var myFunction = new Function("a", "b", "return a * b");
+
 var x = myFunction(4, 3);
 console.log(x);
