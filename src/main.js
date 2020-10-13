c9875853bba3a4bd10bb0338e5231c4598a64dad
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-13 11:13:01 +0200

    Recursive Function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-var myFunction = new Function("a", "b", "return a * b");
-var x = myFunction(4, 3);
-console.log(x);
+function factorial(n) {
+    if (n <= 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(6));
+// ------------------------------------------------
+// Anonymous Recursive Function
+(function () {
+    console.log("Hello World");
+})(); // the function invokes itself using a pair of parentheses ()
