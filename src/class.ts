ccafde7875e67afd3b0b83419205499699ff6e0f
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-20 13:35:42 +0200

    Modules

--- src/class.ts
+++ src/class.ts
@@ -35,3 +35,80 @@ let person1: Person = new Person("Klaus");
 let person2: Person = new Person("Karl");
 
 console.log(person1.getName())
+
+// ----------------------------
+
+class Car {
+  //field
+  engine: string;
+
+  //constructor
+  constructor(engine: string) {
+    this.engine = engine;
+  }
+
+  //function
+  disp(): void {
+    console.log("Engine is: " + this.engine);
+  }
+}
+
+var my_car = new Car("BMW");
+
+//access the field
+console.log("Reading attribute value Engine as :  " + my_car.engine);
+
+//access the function
+my_car.disp();
+
+// -----------------------------
+
+class Shape {
+  area: number;
+
+  constructor(a: number) {
+    this.area = a;
+  }
+}
+
+class Circle extends Shape {
+  disp(): void {
+    console.log("Area of the circle:  " + this.area);
+  }
+}
+
+var c = new Circle(223);
+c.disp();
+
+// ----------------------------------------
+
+class StaticMem {
+  static num: number;
+
+  static disp(): void {
+    console.log("The value of num is: " + StaticMem.num);
+  }
+}
+
+StaticMem.num = 12; // initialize the static variable
+StaticMem.disp(); // invoke the static method
+
+// ----------------------------------------
+
+interface ILoan {
+  interest: number;
+}
+
+class AgriLoan implements ILoan {
+
+  interest: number;
+  rebate: number;
+
+  constructor(interest: number, rebate: number) {
+    this.interest = interest;
+    this.rebate = rebate;
+  }
+}
+
+var o = new AgriLoan(10, 1);
+console.log("Interest is : " + o.interest + " Rebate is : " + o.rebate);
--- src/main.js
+++ src/main.js
@@ -1,37 +1,59 @@
-var customer = {
-    firstName: "Tom",
-    lastName: "Hanks",
-    sayHi: function () {
-        return "Hi there";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Car = /** @class */ (function () {
+    //constructor
+    function Car(engine) {
+        this.engine = engine;
     }
-};
-console.log("Customer Object ");
-console.log(customer.firstName);
-console.log(customer.lastName);
-console.log(customer.sayHi());
-var p = {
-    firstName: "Jim",
-    lastName: "Blakes",
-    sayHi: function () {
-        return "Hello!!!";
+    //function
+    Car.prototype.disp = function () {
+        console.log("Engine is: " + this.engine);
+    };
+    return Car;
+}());
+var my_car = new Car("BMW");
+//access the field
+console.log("Reading attribute value Engine as :  " + my_car.engine);
+//access the function
+my_car.disp();
+// -----------------------------
+var Shape = /** @class */ (function () {
+    function Shape(a) {
+        this.area = a;
     }
-};
-console.log("Employee  Object ");
-console.log(p.firstName);
-console.log(p.lastName);
-//commandline as string
-var options = { program: "test1", commandline: "Hello" };
-console.log(options.commandline);
-//commandline as a string array
-options = { program: "test1", commandline: ["Hello", "World"] };
-console.log(options.commandline[0]);
-console.log(options.commandline[1]);
-//commandline as a function expression
-options = {
-    program: "test1",
-    commandline: function () {
-        return "**Hello World**";
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-};
-var fn = options.commandline;
-console.log(fn());
+    Circle.prototype.disp = function () {
+        console.log("Area of the circle:  " + this.area);
+    };
+    return Circle;
+}(Shape));
+var c = new Circle(223);
+c.disp();
+// ----------------------------------------
+var StaticMem = /** @class */ (function () {
+    function StaticMem() {
+    }
+    StaticMem.disp = function () {
+        console.log("The value of num is" + StaticMem.num);
+    };
+    return StaticMem;
+}());
+StaticMem.num = 12; // initialize the static variable
+StaticMem.disp(); // invoke the static method
